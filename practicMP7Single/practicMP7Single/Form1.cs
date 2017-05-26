1c2ea018c5834cd9e8ad1e1b679791cde39b44b9
MisterX4507 <koryshev1997@gmail.com>
2017-05-27 01:22:12 +0700

    Add files via upload
    Добавление "Одиночки", реализуемой через формы

---
+++ practicMP7Single/practicMP7Single/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practicMP7Single
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = MessageBox.Show("Вы хотите закрыть программу?",
+                "Внимание", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes;
+        }
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormDepended.fw.ShowForm();
+        }
+    }
+}
---
+++ practicMP7Single/practicMP7Single/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practicMP7Single
+{
+    public partial class FormDepended : Form
+    {
+        private FormDepended()
+        {
+            InitializeComponent();
+        }
+
+        private static FormDepended f;
+
+        public static FormDepended getf()
+        {
+           if (f == null || f.IsDisposed) f = new FormDepended();
+           return f;
+        }
+        public static FormDepended fw = getf();
+
+        public void ShowForm()
+        {
+            Show();
+            Activate();
+        }
+    }
+}
